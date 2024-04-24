172dfd4d4b9c7ec3d84d4b83b19524a960f41deb
onboni <onni.luova@gmail.com>
2024-04-24 12:41:40 +0300

    Menun post ja get endpointit

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import express from 'express';
-import {listAllCustomers} from './Model/register-model.js';
+import fs from 'fs';
+import path from 'path';
+import cors from 'cors';
 import bodyParser from 'body-parser';
+
+import {listAllCustomers} from './Model/register-model.js';
+import {registerUser, loginUser} from "./signup.js";
 
 const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
-import cors from 'cors';
-import {registerUser, loginUser} from "./signup.js";
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,6 +32,10 @@ app.get('/users', async (req, res) => {
         console.error(error);
         res.status(500).send({message: 'Virhe asiakkaiden', error: error.message});
     }
+});
+
+app.post('/menu', (req, res) => {
+    res.send('Menu');
 });
 
 app.post('/register', async (req, res) => {
@@ -55,6 +68,29 @@ app.post('/login', async (req, res) => {
         console.error(error);
         res.status(500).send({message: 'Virhe kirjautuessa sisään', error: error.message});
     }
+});
+
+app.get('/menu', (req, res) => {
+    fs.readFile(path.join(__dirname, 'menu.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({message: 'Error reading menu file', error: err.message});
+        } else {
+            res.send(JSON.parse(data));
+        }
+    });
+});
+
+app.post('/menu', (req, res) => {
+    const newMenu = req.body;
+    fs.writeFile(path.join(__dirname, 'menu.json'), JSON.stringify(newMenu, null, 2), 'utf8', (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send({message: 'Virhe', error: err.message});
+        } else {
+            res.send({message: 'Menu päivitetty onnistuneesti'});
+        }
+    });
 });
 
 app.listen(port, hostname, () => {
