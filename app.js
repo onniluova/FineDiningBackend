4869bf1ab92ec32b5d7329fc77dd2ec14240623e
onboni <onni.luova@gmail.com>
2024-04-24 09:50:47 +0300

    Portti muutettu

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ import bodyParser from 'body-parser';
 
 const hostname = '127.0.0.1';
 const app = express();
-const port = 3001;
+const port = 3000;
 import cors from 'cors';
 import {registerUser, loginUser} from "./signup.js";
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get('/users', async (req, res) => {
     try {
